dc076bbff6ca5d2b5b9f9ac34385205b7d5ea0ac
abnatop <abnatop@gmail.com>
2021-01-01 21:32:47 +0900

    Задача 1.

--- task_1.py
+++ task_1.py
@@ -8,24 +8,49 @@
 """
 import re
 
+
 class Data:
     def __init__(self, data_value):
-        self.data_value = data_value
+        if Data.validate(data_value):
+            self.data_value = data_value
 
-    def to_digital(self):
-        data = self.data_value.split('-')
+    @classmethod
+    def to_digital(cls, data_value):
+        return Data._get_digital(data_value)
+
+    @staticmethod
+    def _get_digital(data_value):
+        data = data_value.split('-')
         day = int(data[0])
         month = int(data[1])
         year = int(data[2])
         return day, month, year
 
-    def validate(self):
-        if not isinstance(self.data_value, str):
-            print('NOT valid type (str)')
+    @staticmethod
+    def check_date(data_value):
+        check = Data._get_digital(data_value)
+        if check[0] not in range(1, 32):
+            raise ValueError('Неверное кол-во дней')
+        if check[1] not in range(1, 13):
+            raise ValueError('Неверное кол-во месяцев')
+        return True
+
+    @staticmethod
+    def validate(data_value):
+        if not isinstance(data_value, str):
+            raise ValueError(f'Неверный тип данных {type(data_value)}')
         regex = r"\d{1,2}-\d{1,2}-\d{2,4}"
-        match = re.fullmatch(regex, self.data_value)
-        return True if match else False
+        if not re.fullmatch(regex, data_value):
+            raise ValueError('Неверный формат данных DD-MM-YYYY')
+        if not Data.check_date(data_value):
+            raise ValueError('Неверные данные в дате')
+        return True
 
 
-dd = Data('10-20-3030')
-print(dd.validate())
+sample = '31-12-3030'
+obj = Data(sample)
+
+if Data.validate(sample):
+    print(Data.to_digital(sample))
+else:
+    raise ValueError('Неверный тип данных')
