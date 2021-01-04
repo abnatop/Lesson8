7b7ffc5d144362d0ec9ee91b90b52dffdfdaa6cd
abnatop <abnatop@gmail.com>
2021-01-04 20:13:40 +0900

    Задача 4.

--- task_4.py
+++ task_4.py
@@ -7,10 +7,13 @@
 """
 from abc import ABC, abstractmethod
 
+
 class Warehouse:
     pass
 
+
 class Equipment(ABC):
+    @abstractmethod
     def __init__(self, name, price, count):
         self.name = name
         self.price = price
@@ -41,23 +44,62 @@ class Equipment(ABC):
     def count(self, value):
         self.__count = value
 
+
 class Scanner(Equipment):
+    def __init__(self, name, price, count, resolution):
+        super().__init__(name, price, count)
+        self.resolution = resolution
+
+    @property
+    def resolution(self): return self.__resolution
+
+    @resolution.setter
+    def resolution(self, value):
+        self.__resolution = value
+
     def __str__(self):
-        return f'Сканер {self.name}, цена {self.price}, кол-во {self.count}'
+        return f'Сканер {self.name}, цена {self.price}, ' \
+               f'кол-во {self.count}, разрешение {self.resolution}'
+
 
 class Printer(Equipment):
+    def __init__(self, name, price, count, performance):
+        super().__init__(name, price, count)
+        self.performance = performance
+
+    @property
+    def performance(self): return self.__performance
+
+    @performance.setter
+    def performance(self, value):
+        self.__performance = value
+
     def __str__(self):
-        return f'Принтер {self.name}, цена {self.price}, кол-во {self.count}'
+        return f'Принтер {self.name}, цена {self.price}, кол-во {self.count}, стр/мин {self.performance}'
+
 
 class Copier(Equipment):
+    def __init__(self, name, price, count, color=False):
+        super().__init__(name, price, count)
+        self.color = color
+
+    @property
+    def color(self): return self.__color
+
+    @color.setter
+    def color(self, value):
+        self.__color = value
+
     def __str__(self):
-        return f'Копировальный аппарат {self.name}, цена {self.price}, кол-во {self.count}'
+        is_color = lambda i: 'YES' if i else 'NO'
+        return f'Копировальный аппарат {self.name}, цена {self.price}, ' \
+               f'кол-во {self.count}, цветной {is_color(self.color)}'
 
 
-a = Scanner(name='HP-Scan', price=190.0, count=3)
-b = Printer(name='CANON 12', price=234.0, count=2)
-c = Copier(name='XEROX R5', price=745.0, count=5)
+eq = []
+eq.append(Scanner(name='HP-Scan', price=190.0, count=3, resolution=720))
+eq.append(Printer(name='CANON 12', price=234.0, count=2, performance=24))
+eq.append(Copier(name='XEROX R5', price=745.0, count=5, color=False))
 
-print(a)
-print(b)
-print(c)
+for item in eq:
+    print(item)
