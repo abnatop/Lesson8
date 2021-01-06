102c222844261728cb1106ee07b472ba45f6986c
abnatop <abnatop@gmail.com>
2021-01-06 22:05:04 +0900

    Задача 7.

--- task_7.py
+++ task_7.py
@@ -5,6 +5,8 @@
 сложение и умножение созданных экземпляров. Проверьте корректность полученного
 результата.
 """
+
+
 class Complex:
     def __init__(self, real, imaginary=0):
         self.real = real
@@ -30,12 +32,24 @@ class Complex:
     def imaginary(self, value):
         self.__imaginary = value
 
+    def __add__(self, other):
+        real = self.real + other.real
+        imaginary = self.imaginary + other.imaginary
+        return Complex(real, imaginary)
+
+    def __mul__(self, other):
+        a1, a2 = self.real, self.imaginary
+        b1, b2 = other.real, other.imaginary
+        real, imaginary = a1 * b1 - a2 * b2, a1 * b2 + a2 * b1
+        return Complex(real, imaginary)
+
     def __str__(self):
         sign = lambda s: '+' if s >= 0 else ''
         return f'{self.real}{sign(self.imaginary)}{self.imaginary}i'
 
-d = Complex(3, -5)
 
-print(d.real, d.imaginary)
-print(d.complex)
-print(d)
+d = Complex(1, -1)
+g = Complex(3, 6)
+
+print(d + g)
+print(d * g)
