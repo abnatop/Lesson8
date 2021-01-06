430e76fa9652a434415f402da0dcf468a01f4d01
abnatop <abnatop@gmail.com>
2021-01-06 18:00:01 +0900

    Задача 4.

--- task_5.py
+++ task_5.py
@@ -3,30 +3,41 @@
 оргтехники на склад и передачу в определенное подразделение компании. Для хранения
 данных о наименовании и количестве единиц оргтехники, а также других данных, можно
 использовать любую подходящую структуру, например словарь.
-incoming - приход
-outgoing - расход
 """
 from abc import ABC, abstractmethod
 from random import choice
 from string import ascii_uppercase, ascii_lowercase, digits
+
 
 class Warehouse:
     def __init__(self, name):
         self.name = name
         self.__content = []
 
-    def add_item(self, item):
-        self.__content.append(item)
-
     @property
-    def content(self): return self.__content
-
-    @property
-    def name(self): return self.__name
+    def name(self):
+        return self.__name
 
     @name.setter
     def name(self, value):
         self.__name = value
+
+    @property
+    def content(self):
+        return self.__content
+
+    @property
+    def quantity(self):
+        return len(self.__content)
+
+    def add_item(self, item):
+        self.__content.append(item)
+
+    def delete_item(self, item_id):
+        for index, item in enumerate(self.__content):
+            if item_id == item.uniq_id:
+                return self.__content.pop(index)
+
 
 class Equipment(ABC):
     @staticmethod
@@ -65,6 +76,7 @@ class Equipment(ABC):
     def uniq_id(self, value):
         self.__uniq_id = value
 
+
 class Scanner(Equipment):
     def __init__(self, name, price, resolution):
         super().__init__(name, price)
@@ -81,6 +93,7 @@ class Scanner(Equipment):
         return f'ID {self.uniq_id} - Сканер {self.name}, ' \
                f'цена {self.price}, разрешение {self.resolution}'
 
+
 class Printer(Equipment):
     def __init__(self, name, price, performance):
         super().__init__(name, price)
@@ -96,6 +109,7 @@ class Printer(Equipment):
     def __str__(self):
         return f'ID {self.uniq_id} - Принтер {self.name}, ' \
                f'цена {self.price}, стр/мин {self.performance}'
+
 
 class Copier(Equipment):
     def __init__(self, name, price, color=False):
@@ -114,14 +128,32 @@ class Copier(Equipment):
         return f'ID {self.uniq_id} - Копировальный аппарат {self.name}, ' \
                f'цена {self.price}, цветной {is_color(self.color)}'
 
-wh = Warehouse('WH1')
+
+wh1 = Warehouse('WH1')
+wh2 = Warehouse('WH2')
+
 eq = []
 eq.append(Scanner(name='HP-Scan', price=190.0, resolution=720))
+eq.append(Scanner(name='Pixma', price=140.0, resolution=640))
 eq.append(Printer(name='CANON 12', price=234.0, performance=24))
+eq.append(Printer(name='EPSON 890', price=345.0, performance=18))
+eq.append(Printer(name='BROTHER 4T', price=468.0, performance=10))
 eq.append(Copier(name='XEROX R5', price=745.0, color=False))
 
 for item in eq:
-    wh.add_item(item)
+    wh1.add_item(item)  # приход товара на склад
 
-for item in wh.content:
+goods_id = wh1.content[2].uniq_id  # проверочный id
+
+goods = wh1.delete_item(goods_id)  # перемещение товара со склада wh1
+wh2.add_item(goods)  # на склад wh2
+
+print(f'=> Склад {wh1.name}, всего товаров {wh1.quantity}')
+for item in wh1.content:
+    print(item)
+
+print()
+
+print(f'=> Склад {wh2.name}, всего товаров {wh2.quantity}')
+for item in wh2.content:
     print(item)
